63a7bcdc898d7f9efde7d04515f073897b3b8620
asset89 <asset89@gmail.com>
2021-12-24 20:24:58 +0600

    Initial Commit

---
+++ Geo_Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Geo_Task
+//
+//  Created by Asset Ryskul on 24.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Geo_TaskUITests/Geo_TaskUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Geo_TaskUITestsLaunchTests.swift
+//  Geo_TaskUITests
+//
+//  Created by Asset Ryskul on 24.12.2021.
+//
+
+import XCTest
+
+class Geo_TaskUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
